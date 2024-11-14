76b4c5903929a79c60bc41e52451e4b95571b96a
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-11-13 21:26:41 -0300

    feat: implementando componente de notificacoes lidas

--- src/app/notificacoes-lidas/notificacoes-lidas.component.ts
+++ src/app/notificacoes-lidas/notificacoes-lidas.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { listaNotificacaoSelector } from '../state/app.selectors';
+import { Notificacao } from '../models/notification.model';
+import { Store } from '@ngrx/store';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-notificacoes-lidas',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrl: './notificacoes-lidas.component.scss'
 })
 export class NotificacoesLidasComponent {
+
+  notificacoesSelector = this.store.select(listaNotificacaoSelector);
+  listaNotificacoes: Notificacao[] = [];
+
+  constructor(private store: Store) {
+
+  }
+
+  ngOnInit(): void {
+    this.notificacoesSelector.pipe(map(listaDoEstadoMisturada => {
+      return listaDoEstadoMisturada.filter(notificacao => notificacao.lida)
+    })).subscribe({
+      next: (listaDoEstadoTransformada) => {
+        // for(let elemento of listaDoEstado) {
+        //   if(elemento.lida) {
+        //     this.listaNotificacoes.push(elemento);
+        //   }
+        // }
+
+        // this.listaNotificacoes = listaDoEstado.filter(notificacao => notificacao.lida);
+        this.listaNotificacoes = listaDoEstadoTransformada
+
+        console.log(this.listaNotificacoes);
+      }
+    })
+  }
 
 }
